a1825d0f666d7d8f06a4ba7aa5fb24061410f9c5
libits <1002846237@qq.com>
2023-08-01 16:16:13 +0800

    delete

--- 以太坊/Web3 DeFi项目资料/017-034 代码笔记/code/src/main.js
+++
@@ -1,4 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-const vm = createApp(App);
-vm.mount("#app");
--- 以太坊/Web3 DeFi项目资料/051-064 代码笔记/code/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-const NodePolyfillWebpackPlugin = require("node-polyfill-webpack-plugin")
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-  configureWebpack: {
-    plugins:[new NodePolyfillWebpackPlugin()]
-  }
-});
